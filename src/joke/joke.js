30ae494466ff0334acb58e9364e50104c50b64bb
Adrian <adriandinca2008@yahoo.com>
2018-01-12 22:05:34 +0200

    added the copy method for coping a joke

--- src/joke/joke.js
+++ src/joke/joke.js
@@ -1,3 +1,7 @@
+import createElement from "../createElement/createElement";
+
+const HIDDEN_INPUT_ID = 'hidden-copy-input';
+
 class Joke {
     /**
      * @param {string} title
@@ -6,6 +10,22 @@ class Joke {
     constructor(text, votes) {
         this.text = text;
         this.votes = votes;
+        // we set this input's value to the joke's then copy it
+        this._hiddenCopyInput = document.getElementById(HIDDEN_INPUT_ID);
+    }
+
+    // copies a joke's text
+    copy() {
+        if (!this._hiddenCopyInput) {
+            this._hiddenCopyInput = document.createElement('input');
+            this._hiddenCopyInput.type = "text";
+            this._hiddenCopyInput.id = HIDDEN_INPUT_ID;
+            document.body.appendChild(this._hiddenCopyInput);
+        }
+
+        this._hiddenCopyInput.value = this.text;
+        this._hiddenCopyInput.select();
+        document.execCommand("copy");
     }
 }
 
