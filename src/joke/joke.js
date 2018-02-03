5f71e0af73cf293c5f89f9c0b2d5997e1f28fde0
Adrian <adriandinca2008@yahoo.com>
2018-02-03 22:09:01 +0200

    removed the createSnackbar function inside the joke class since now we have a snackbarView, and renamed the copy function to copyToClipboard

--- src/joke/joke.js
+++ src/joke/joke.js
@@ -1,8 +1,8 @@
 import createElement from "./../createElement/createElement";
 import componentHandler from "./../mdlComponentHandler";
+import snackbarView from "./../snackbarView/snackbarView"
 
 const HIDDEN_INPUT_ID = 'hidden-copy-input';
-const SNACKBAR_ID = "snackbar";
 
 class Joke {
     /**
@@ -15,12 +15,10 @@ class Joke {
 
         /** @private we set this input's value to the joke's then copy it */
         this._hiddenCopyInput = document.getElementById(HIDDEN_INPUT_ID);
-        /** @private used to let the user know they've copied the joke */
-        this._snackBar = document.getElementById(SNACKBAR_ID);
     }
 
     // copies a joke's text
-    copy() {
+    copyToClipboard() {
         if (!this._hiddenCopyInput) {
             this._createHiddenCopyInput();
         }
@@ -29,11 +27,7 @@ class Joke {
         this._hiddenCopyInput.select();
         document.execCommand("copy");
 
-        if (!this._snackBar) {
-            this._createSnackbar();
-        }
-
-        this._snackBar.MaterialSnackbar.showSnackbar({
+        snackbarView.showSnackbar({
             message: 'Joke copied to clipboard.'
         });
     }
@@ -45,25 +39,6 @@ class Joke {
         this._hiddenCopyInput.type = "text";
         this._hiddenCopyInput.id = HIDDEN_INPUT_ID;
         document.body.appendChild(this._hiddenCopyInput);
-    }
-
-    /** @private creates snackbar with an id of SACKBAR_ID and adds it to the DOM */
-    _createSnackbar() {
-        // <div id="snackbar" class="mdl-js-snackbar mdl-snackbar">
-        this._snackBar = createElement('div', 'mdl-js-snackbar mdl-snackbar');
-        this._snackBar.id = SNACKBAR_ID;
-        document.body.appendChild(this._snackBar);
-
-        // <div class="mdl-snackbar__text"></div>
-        let snackbarText = createElement('div', 'mdl-snackbar__text');
-        this._snackBar.appendChild(snackbarText);
-
-        // <button type="button" class="mdl-snackbar__action"></button>
-        let snackbarAction = createElement('button', 'mdl-snackbar__action');
-        snackbarAction.type = "button";
-        this._snackBar.appendChild(snackbarAction);
-
-        componentHandler.upgradeElements(this._snackBar);
     }
 }
 
