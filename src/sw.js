254e9ef9bbfb610029070ec5236d9e736fd1ab89
Adrian <adriandinca2008@yahoo.com>
2018-02-03 22:11:54 +0200

    added a service worker which caches the most important files and notifies the user when an update is available.

--- src/sw.js
+++ src/sw.js
@@ -1,1 +1,24 @@
-self.addEventListener('fetch', event => console.log(event.request));
+self.addEventListener('install', event => event.waitUntil(
+    caches.open('jokr-static-v1')
+        .then(cache => cache.addAll([
+            '/',
+            'main.min.css',
+            'jokr.bundle.min.js',
+            'images/buffer.svg',
+            'images/tick-mask.svg',
+            'images/tick.svg',
+            'https://fonts.googleapis.com/icon?family=Material+Icons',
+            'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700'
+        ]))
+));
+
+self.addEventListener('message', event => {
+    if (event.data.action == 'skipWaiting') {
+        self.skipWaiting();
+    }
+});
+
+self.addEventListener('fetch', event => event.respondWith(
+    caches.match(event.request)
+        .then(response => response || fetch(event.request))
+));
