b57310c9ffc17867b77ef3d8077071050b1a8b21
Adrian <adriandinca2008@yahoo.com>
2018-02-11 21:12:54 +0200

    commented all the import satements for the components which are not used for now

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import componentHandler from './mdlComponentHandler';
 import MaterialButton from './button/button';
-import MaterialCheckbox from './checkbox/checkbox';
-import MaterialDataTable from './data-table/data-table';
+// import MaterialCheckbox from './checkbox/checkbox';
+// import MaterialDataTable from './data-table/data-table';
 import MaterialIconToggle from './icon-toggle/icon-toggle';
 import MaterialLayout from './layout/layout';
-import MaterialMenu from './menu/menu';
-import MaterialProgress from './progress/progress';
-import MaterialRadio from './radio/radio';
+// import MaterialMenu from './menu/menu';
+// import MaterialProgress from './progress/progress';
+// import MaterialRadio from './radio/radio';
 import MaterialRipple from './ripple/ripple';
-import MaterialSlider from './slider/slider';
+// import MaterialSlider from './slider/slider';
 import MaterialSnackbar from './snackbar/snackbar';
-import MaterialSpinner from './spinner/spinner';
-import MaterialSwitch from './switch/switch';
-import MaterialTabs from './tabs/tabs';
-import MaterialTextfield from './textfield/textfield';
+// import MaterialSpinner from './spinner/spinner';
+// import MaterialSwitch from './switch/switch';
+// import MaterialTabs from './tabs/tabs';
+// import MaterialTextfield from './textfield/textfield';
 import rAF from './third_party/rAF';
-import MaterialTooltip from './tooltip/tooltip';
+// import MaterialTooltip from './tooltip/tooltip';
 import IndexController from './indexController';
 
 new IndexController(document.getElementById('latest-jokes'))
